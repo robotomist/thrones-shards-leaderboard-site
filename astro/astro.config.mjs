636c048d82d552c3d8379f5ea02b131ccaef4e76
robotomist <robotomist@users.noreply.github.com>
2024-01-25 19:05:32 -0800

    maybe it works now?

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://robotomist.github.io',
-  base: '/thrones-shards-leaderboard-site',
+  base: '/',
   integrations: [tailwind()]
 });
