255e91469c3476ce482ff6facc88cbce5cbf6b13
robotomist <robotomist@users.noreply.github.com>
2024-01-25 16:13:45 -0800

    adjust astro config

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://robotomist.github.io',
+  base: '/astro',
   integrations: [tailwind()]
 });
